71da6e250b7f2345a5ab47c9a6a258f1f4c4f9d7
davidyisun <david_yisun@163.com>
2018-06-14 10:48:03 +0800

    +表格转换整理

---
+++ table_preprocess.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+    脚本名: 表格预处理
+Created on 2018-06-13
+@author:David Yisun
+@group:data
+@contact:davidhu@wezhuiyi.com
+"""
---
+++ table_preprocess_single.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+    脚本名: 表格预处理单个表格
+Created on 2018-06-13
+@author:David Yisun
+@group:data
+@contact:davidhu@wezhuiyi.com
+"""
+
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+    脚本名:单个html test
+Created on 2018-06-12
+@author:David Yisun
+@group:data
+@contact:davidhu@wezhuiyi.com
+"""
+import codecs
+from bs4 import BeautifulSoup
+import os
+import re
+import copy
+
+
+catalogue = '增减持'
+path = './data/round2_adjust/{0}/html/'.format(catalogue)
+file = path+'7544.html'
+# --- 单个html ---
+with codecs.open(file, 'r', 'utf8' ) as f:
+    data = f.read()
+data = re.sub(re.compile('>\n* *<'), '><', data)
+data = re.sub(re.compile('\n'), '', data)
+d = BeautifulSoup(data, 'lxml', from_encoding='utf-8')
+
+tables = d.find_all('table')
+table = tables[0].tbody
+
+# 获取行总数
+t = table.contents
+n_row = len(t)
+
+import pandas as pd
+d1 = pd.read_html(file, header=0)
+df = pd.DataFrame(d1)
+
+
+
+
