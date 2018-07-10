f362a93f4e8b62c3059610541f986a13001751ef
=yisun <=>
2018-07-10 21:23:13 +0800

    +完成增减持表格信息抽取脚本

--- extract/increase_or_decrease.py
+++ extract/increase_or_decrease.py
@@ -23,8 +23,8 @@ def get_content(has_table=True):
     # outpath = 'D:\\TianChi_competition\\公告信息抽取\\materials\\数据\\outpath\\train\\increase_or_decrease\\'
     # 本地测试
     # path = 'D:\\TianChi_competition\\公告信息抽取\\materials\\数据\\训练数据\\round1_train_20180518\\增减持\\html\\'
-    # filename = '20526193.html'
-    outpath = './data/temp/'
+    # filename = '18862885.html'
+    # outpath = './data/temp/'
     html_dict = convert.read_html2(filepath=path, filename=filename)
     contents = {}
     for n, index in enumerate(html_dict):
@@ -48,8 +48,8 @@ def extract_table(table_dict):
     reg_holders = [re.compile('股东名称|股东姓名|姓名|股东及其一致行动人名称'), re.compile('星星点灯')]
     reg_price = [re.compile('价格|均价|元'), re.compile('总.*额')]
     reg_amount = [re.compile('股数|数量'), re.compile('前|后|持有股数|持股数量|持有数量|比例*')]
-    reg_amount_later = [re.compile('后持有+.*股*数量*|[增减]持[后]+.*股*数量*|当前持+.*股*数量*'), re.compile('比例*')]
-    reg_amount_ratio_later = [re.compile('后持有+.*比例*|[增减]持[前后]+.*比例|当前持+.*比例'), re.compile('星星点灯')]
+    reg_amount_later = [re.compile('后持有.*股*数量*|[增减]持后.*股*数量*|当前持.*股*数量*'), re.compile('比例*')]
+    reg_amount_ratio_later = [re.compile('后持有.*比例*|[增减]持后.*比例|当前持.*比例'), re.compile('星星点灯')]
     reg_method = [re.compile('持方式'), re.compile('星星点灯')]  # 增减持方式
     reg_share_nature = [re.compile('性质'), re.compile('星星点灯')] # 股份性质
     # 正则字典
