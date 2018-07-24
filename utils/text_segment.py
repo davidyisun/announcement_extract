ae5de069d41185428cf9aa9c0e5f428768768f08
=yisun <=>
2018-07-24 13:38:04 +0800

    +优化【释义】部分的筛选

---
+++ utils/text_segment.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+    脚本名: 文本拆分
+    将文本拆分成 【释义】、【重大提示】、【正文】
+Created on 2018-07-24
+@author:David Yisun
+@group:data
+"""
+def save_shiyi():
+    pass
