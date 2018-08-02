bcd26ccfafa5675f25cdfdfcee9e91a0fd883288
=yisun <=>
2018-08-02 12:20:28 +0800

    +增加 【评估方法】的定位模块

--- scripts/chongzu_extract.py
+++ scripts/chongzu_extract.py
@@ -16,6 +16,7 @@ import os
 import math
 import codecs
 import json
+import copy
 
 
 def from_shiyi(path, filename, outpath, result_append=True):
@@ -84,12 +85,17 @@ def from_content(path, filename, outpath, result_append=True, title_depth=0):
 def extract_method_from_content(path, filename, outpath):
     data = chongzu.get_pre_content(path=path, filename=filename, keys=['content'], text_trans=True, df_json=True)
     result = {}
-    reg = '收益法|资产基础法'
+    # reg = '收益法|资产基础法|成本分析法|市场比较法|市场法|收益法|'
+    reg = '收益法|资产基础法|市场法|市场比较法|估值法|成本法|现金流折现法|内含价值调整法|' \
+          '可比公司市净率法|重置成本法|收益现值法|基础资产法|假设清偿法|成本逼近法|单项资产加和法|' \
+          '成本加和法|基准地价修正法|收益还原法|现金流量法|成本发|单项资产加总法|折现现金流量法'
     for i, index in enumerate(data):
         print('extracting method from content --- total: {0} --- this: {1} --- file: {2}'.format(len(data), i, index))
         content = data[index]['content']
         ExtractDevice = chongzu.ExtractDevice(content_list=content)
         _res = ExtractDevice.get_tree_content_on_text(reg_object=reg)
+        if _res == []:
+            continue
         result[index.replace('.html', '')] = _res
     out = json.dumps(result)
     with codecs.open(outpath+'method_text.csv', 'a', 'utf8') as f:
@@ -129,11 +135,15 @@ def main(postfix='.html', batches=20):
     # outpath = '../data/extract_result/train_chongzu/'
 
     # --- 本地数据 ---
-    # path = '../data/temp2/samples/'
-    # filename = None
+    path = '../data/temp2/samples/'
+    filename = None
     # filename = ['8515.html']
-    # outpath = '../data/extract_result/train_chongzu/'
-    # label_file = '../data/extract_result/train_chongzu/'
+    outpath = '../data/extract_result/train_chongzu/'
+    label_file = '../data/train_data/train_labels/chongzu.train'
+
+    # --- label 字段 ---
+    headers = ['id', 'mark', 'mark_com', 'jiaoyiduifang', 'price', 'method']
+    df_label = result_compare.get_labels(file=label_file+'', headers=headers)
 
     if filename == None:
         files_name = os.listdir(path)
@@ -154,12 +164,33 @@ def main(postfix='.html', batches=20):
             break
         # --- 从【释义】抽取 ---
         # from_shiyi(path=path, outpath=outpath, filename=_file_list)
+
         # --- 从【正文】抽取 ---
         # r1 = from_content(path=path, outpath=outpath, filename=_file_list, title_depth=0)
+
         # --- 从【正文】中定位【评估方法】 ---
         res = extract_method_from_content(path=path, outpath=outpath, filename=_file_list)
+        extract_id = res.keys()
+        df = df_label.dropna(subset=['method']).reset_index(drop=True).copy()
+
+        # --- 基本评测 ---
+        label_id = [str(i) for i in df['id'].unique().tolist()]
+        not_in_id = copy.deepcopy(label_id)  # 缺失
+        in_id = []
+        out_of_id = []  # 多余
+
+        in_id = in_id+list(set(label_id).intersection(set(extract_id)))
+        not_in_id = list(set(not_in_id).difference(set(extract_id)))
+        out_of_id = out_of_id+list(set(extract_id).difference(set(label_id)))
         batch_head = batch_head+batches
         print('total: {0} --- has been processed: {1}'.format(len(file_list), min(len(file_list), batch_head)))
+    with codecs.open(outpath+'stat_result.txt', 'w', 'utf8') as f:
+        f.write('------------------------\n')
+        f.write('not_in_id: {0}'.format(len(not_in_id))+'\n')
+        f.write(' '.join(not_in_id))
+        f.write('\n------------------------\n')
+        f.write('out_of_id: {0}'.format(len(out_of_id))+'\n')
+        f.write(' '.join(out_of_id))
     return
 
 
